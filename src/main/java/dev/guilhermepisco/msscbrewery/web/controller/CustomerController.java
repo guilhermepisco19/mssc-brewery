656dbff8869b2ae6ccc8538c8833b81cc21856b6
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2023-07-13 12:12:00 +0100

    Customer POST, PUT and DELETE

--- src/main/java/dev/guilhermepisco/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/dev/guilhermepisco/msscbrewery/web/controller/CustomerController.java
@@ -2,9 +2,12 @@ package dev.guilhermepisco.msscbrewery.web.controller;
 
 import dev.guilhermepisco.msscbrewery.web.model.CustomerDto;
 import dev.guilhermepisco.msscbrewery.web.service.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -26,5 +29,33 @@ public class CustomerController {
                 .body(customerService.getCustomerById(customerId));
 
     }
+
+    @PostMapping
+    public ResponseEntity<Void> handlePost(@RequestBody CustomerDto customerDto){
+
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(savedDto.customerId()).toUri();
+
+        return ResponseEntity
+                .created(uri)
+                .build();
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Void> handlePut(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto){
+
+        customerService.updateCustomer(customerId, customerDto);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable UUID customerId){
+        customerService.deleteCustomerById(customerId);
+    }
+
 
 }
--- src/main/java/dev/guilhermepisco/msscbrewery/web/service/CustomerServiceImpl.java
+++ src/main/java/dev/guilhermepisco/msscbrewery/web/service/CustomerServiceImpl.java
@@ -1,14 +1,31 @@
 package dev.guilhermepisco.msscbrewery.web.service;
 
 import dev.guilhermepisco.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
         return new CustomerDto(UUID.randomUUID(), "John");
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return new CustomerDto(UUID.randomUUID(), "Bob");
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        log.info("Updating Customer ...");
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+        log.info("Deleting Customer ...");
     }
 }
